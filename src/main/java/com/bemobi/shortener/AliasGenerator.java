fae6292b11dd2eeea4b5b944f26a97ea08f2f7f1
Ciro Xavier <ciroprates@gmail.com>
2016-03-30 22:43:31 -0300

    AliasGenerator, cara!

---
+++ src/main/java/com/bemobi/shortener/AliasGenerator.java
@@ -0,0 +1,35 @@
+package com.bemobi.shortener;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by ciroxavier on 3/30/16.
+ */
+public class AliasGenerator {
+
+    private final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    private final Random RANDOM = new Random();
+
+    private final int ALIAS_SIZE = 6;
+
+    public String generateAlias() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ALIAS_SIZE; i++) {
+            int charIndex = RANDOM.nextInt(ALPHANUMERIC.length() - 1);
+            sb.append(ALPHANUMERIC.toCharArray()[charIndex]);
+        }
+
+        return sb.toString();
+    }
+
+    public static String buildAlphanumericString() {
+        IntStream alphaStream = IntStream.concat(IntStream.rangeClosed(65, 90), IntStream.rangeClosed(97, 122));
+        IntStream alphaNumericStream = IntStream.concat(alphaStream, IntStream.rangeClosed(48, 57));
+        Stream<String> stringStream = alphaNumericStream.mapToObj(a -> String.valueOf((char) a));
+        return stringStream.reduce(String::concat).get();
+    }
+
+}
